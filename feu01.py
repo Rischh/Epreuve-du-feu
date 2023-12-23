b3e3d94ace274a30c89caf2f499e1597aaa4975e
samuel-risch <rischebourg.samuel@gmail.com>
2023-12-23 17:18:53 +0100

    Reussi a separer la parenthese

--- feu01.py
+++ feu01.py
@@ -6,7 +6,7 @@ def find_op(arith, op):
     arith_list = arith.split()
     index = [n for (n, e) in enumerate(arith_list) if e == op]
     return index[0]
-        
+
 def behind(op):
     behind = op - 1
     return behind
@@ -18,16 +18,29 @@ def front(op):
 def result_of(arith):
     arith_list = arith.split()
     
-    while "+" in arith_list:
-        operator = '+'
-                            
+    if '(' in arith_list:
+        parenthesis_index = []
+        
+        operator = '('     
         op_index = find_op(arith,operator)
-        b = behind(op_index)
-        f = front(op_index)
-                            
-        calcul = int(arith_list[b]) + int(arith_list[f])
-        arith_list = arith_list[:b] + [str(calcul)] + arith_list[f+1:]
-                    
-    print(arith_list)
-            
+        parenthesis_index.append(op_index)
+
+        operator = ')'
+        op_index = find_op(arith,operator)
+        parenthesis_index.append(op_index)
+        
+        sub_expression = arith_list[parenthesis_index[0] + 1:parenthesis_index[1]]
+
 result_of(arithmetic_progression)
+
+# while "+" in arith_list:
+    #     operator = '+'
+                            
+    #     op_index = find_op(arith,operator)
+    #     b = behind(op_index)
+    #     f = front(op_index)
+                            
+    #     calcul = int(arith_list[b]) + int(arith_list[f])
+    #     arith_list = arith_list[:b] + [str(calcul)] + arith_list[f+1:]
+                    
+    # print(arith_list)
