304794de199f453a858117621c0173f690fb9249
Rischh <rischebourg.samuel@gmail.com>
2025-05-05 23:57:48 +0200

    feat: feu03 nearly done

---
+++ feu03.js
@@ -0,0 +1,148 @@
+const fs = require("node:fs")
+
+const solveSudoku = (grid, row, cell) => {
+  if (row === 9) return true
+
+  if (cell === 9) return solveSudoku(grid, ++row, 0)
+
+  if (grid[row][cell] !== ".") return solveSudoku(grid, row, ++cell)
+
+  for (let k = 1; k <= 9; k++) {
+    if (isValidCell(grid, row, cell, k)) {
+      grid[row][cell] = k
+
+      if (solveSudoku(grid, row, ++cell)) return true
+      grid[row][cell] = "."
+    }
+  }
+  return false
+}
+
+const isValidCell = (grid, row, cell, k) => {
+  const notInRow = !grid[row].includes(k)
+
+  let notInColumn = true
+  for (let i = 0; i < 9; i++) {
+    if (grid[i][cell] === k) {
+      notInColumn = false
+      break
+    }
+  }
+
+  let notInSubgrid = true
+  // for (
+  //   let j = Math.floor((row / 3) * 3);
+  //   j < Math.floor((row / 3) * 3 + 3);
+  //   j++
+  // ) {
+  //   for (
+  //     let l = Math.floor((cell / 3) * 3);
+  //     l < Math.floor((cell / 3) * 3 + 3);
+  //     l++
+  //   ) {
+  //     console.log(grid[j][l])
+  //     if (grid[j][l] === k) {
+  //       notInSubgrid = false
+  //       break
+  //     }
+  //   }
+  // }
+
+  return notInRow && notInColumn && notInSubgrid
+}
+
+const getContentFile = (fileName) => {
+  const content = fs.readFileSync(`./${fileName}`, "utf8")
+  return content
+}
+
+const getMatrixSudoku = (sudoku) => {
+  const matrixSudoku = sudoku.split("\n").map((string) => string.split(""))
+
+  const stringDigits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  for (let i = 0; i < matrixSudoku.length; i++) {
+    for (let j = 0; j < matrixSudoku[i].length; j++) {
+      const cell = matrixSudoku[i][j]
+      if (stringDigits.includes(cell)) {
+        matrixSudoku[i][j] = +matrixSudoku[i][j]
+      }
+    }
+  }
+
+  return matrixSudoku
+}
+
+const isValidArgsLength = (args, wantedLength) => {
+  if (args.length !== wantedLength)
+    return console.error(
+      `Le programme a besoin de ${wantedLength} pour fonctionner.`
+    )
+  return args
+}
+
+const isValidString = (string) => {
+  if (!isNaN(string))
+    return console.error(
+      "Le programme a besoin d'une chaine de caracteres comme argument pour fonctionner."
+    )
+  return string
+}
+
+const isValidFileExtension = (fileName, validFileExtension) => {
+  const fileExtension = fileName.split(".")[1]
+
+  if (fileExtension !== validFileExtension)
+    return console.error("L'extension du fichier n'est pas valide.")
+  return fileName
+}
+
+const isExistingFile = (filePath) => {
+  const isExisting = fs.existsSync(filePath)
+
+  if (!isExisting)
+    return console.error("Le fichier n'existe pas dans le dossier courrant.")
+  return true
+}
+
+const isReadableFile = (filePath) => {
+  try {
+    fs.accessSync(filePath, fs.constants.R_OK)
+    return true
+  } catch (error) {
+    console.error("Le fichier specifie n'est pas lisible.")
+    return false
+  }
+}
+
+const getArgs = () => {
+  const args = process.argv.slice(2)
+  return args
+}
+
+const displayResolvedSudoku = () => {
+  const args = isValidArgsLength(getArgs(), 1)
+  if (!args) return
+
+  const string = isValidString(args[0])
+  if (!string) return
+
+  const fileName = isValidFileExtension(string, "txt")
+  if (!fileName) return
+
+  if (!isExistingFile(fileName)) return
+  if (!isReadableFile(fileName)) return
+
+  const sudoku = getContentFile(fileName)
+
+  const matrixSudoku = getMatrixSudoku(sudoku)
+
+  const result = [...matrixSudoku]
+  solveSudoku(result, 0, 0)
+
+  return result
+    .map((array) => array.join(""))
+    .forEach((element) => console.log(element))
+}
+
+displayResolvedSudoku()
